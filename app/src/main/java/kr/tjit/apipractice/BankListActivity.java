72545d45487eea5be1ae3ebdeb5e1d1157e63436
manymany.macarons <manymany.macarons@gmail.com>
2019-01-13 12:40:16 +0900

    BankAdapter 작성 완료

--- app/src/main/java/kr/tjit/apipractice/BankListActivity.java
+++ app/src/main/java/kr/tjit/apipractice/BankListActivity.java
@@ -64,6 +64,10 @@ public class BankListActivity extends BaseActivity {
 
                         }
                     }
+
+
+
+
                     else {
 
                     }
--- app/src/main/java/kr/tjit/apipractice/datas/Bank.java
+++ app/src/main/java/kr/tjit/apipractice/datas/Bank.java
@@ -8,9 +8,11 @@ import java.io.Serializable;
 public class Bank implements Serializable {
 
     private int id;
-    private int code;
+    private String code;
     private String bankName;
     private String logoUrl;
+
+
 
     public static Bank getBankFromJson(JSONObject bankJson) {
         Bank bank = new Bank();
@@ -18,7 +20,7 @@ public class Bank implements Serializable {
         try {
 
             bank.setId(bankJson.getInt("id"));
-            bank.setCode(bankJson.getInt("code"));
+            bank.setCode(bankJson.getString("code"));
             bank.setBankName(bankJson.getString("name"));
             bank.setLogoUrl(bankJson.getString("logo"));
 
@@ -38,16 +40,16 @@ public class Bank implements Serializable {
         this.id = id;
     }
 
-    public int getCode() {
+    public String getBankName() {
+        return bankName;
+    }
+
+    public String getCode() {
         return code;
     }
 
-    public void setCode(int code) {
+    public void setCode(String code) {
         this.code = code;
-    }
-
-    public String getBankName() {
-        return bankName;
     }
 
     public void setBankName(String bankName) {
